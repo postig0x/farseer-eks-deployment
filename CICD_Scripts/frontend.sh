b2caa0abdcf6b4b807028c752adac54b87957a24
Dolma <dolmatgurung1@gmail.com>
2024-12-02 16:03:57 -0500

    manual installation of node js

--- CICD_Scripts/frontend.sh
+++ CICD_Scripts/frontend.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash
-sudo apt install -y nodejs
+# curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash
+# sudo apt install -y nodejs
 node -v
 npm i
 npm run build
