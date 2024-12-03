fb0d8f1da92341e6527ba3dd6f6ae4cb0dcde20c
jonwang22 <jon.wang2@gmail.com>
2024-12-03 17:49:17 -0500

    fixed cidr block on private subnet in dev env

--- farseer/Terraform/Dev/modules/VPC/main.tf
+++ farseer/Terraform/Dev/modules/VPC/main.tf
@@ -93,7 +93,7 @@ resource "aws_route_table_association" "pub-rt-association" {
 #############################################################################
 resource "aws_subnet" "private-subnet" {
   vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1a"
   tags = {
     Name = "${var.environment}-private-subnet"
