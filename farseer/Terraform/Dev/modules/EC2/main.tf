1ec72a12bc5b257451a1038bf743b5818501578a
jonwang22 <jon.wang2@gmail.com>
2024-12-03 18:29:04 -0500

    fixing up some more dev env things

--- farseer/Terraform/Dev/modules/EC2/main.tf
+++ farseer/Terraform/Dev/modules/EC2/main.tf
@@ -40,7 +40,7 @@ resource "aws_security_group" "frontend_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/24"]
   }
 
   ingress {
