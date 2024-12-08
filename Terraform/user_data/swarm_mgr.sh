bbea43caceb8260e9e099e7bd38e7f15fdbdf84d
jonwang22 <jon.wang2@gmail.com>
2024-12-07 22:43:24 -0500

    fixing nginx commands

--- Terraform/user_data/swarm_mgr.sh
+++ Terraform/user_data/swarm_mgr.sh
@@ -69,14 +69,20 @@ sudo apt install -y nginx
 sleep 3
 echo "slept 3 after installing nginx"
 
-# modify nginx config (/etc/nginx/sites-available/default)
-# sudo sed -i "/location \/ {/,/}/c\    location / {\n        proxy_pass http://${front_ip}:3000;\n        proxy_set_header Host $host;\n        proxy_set_header X-Real-IP $proxy_add_x_forwarded_for;\n    }" /etc/nginx/sites-enabled/default
+# modify nginx config (/etc/nginx/sites-enabled/default)
+# sudo sed -i "/location \/ {/,/}/{s|.*|    location / {\n        proxy_pass http://${front_ip}:3000;\n        proxy_set_header Host \$host;\n        proxy_set_header X-Real-IP \$proxy_add_x_forwarded_for;\n    }|; q}" /etc/nginx/sites-enabled/default
+sudo sed -i "0,/location \/ {/,/}/{c\
+    location / {\n\
+        proxy_pass http://${front_ip}:3000;\n\
+        proxy_set_header Host \$host;\n\
+        proxy_set_header X-Real-IP \$proxy_add_x_forwarded_for;\n\
+    }" /etc/nginx/sites-enabled/default
 
 echo "nginx config modified"
 
 # start and enable nginx
-# sudo systemctl start nginx
-# sudo systemctl enable nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
 
 echo "nginx started and enabled"
 
