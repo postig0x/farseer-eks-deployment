9bc5450006537f0493e31f5e92ba1bd367487762
Dolma <dolmatgurung1@gmail.com>
2024-12-11 23:38:43 -0500

    recreate infra

--- Terraform/sb/main.tf
+++ Terraform/sb/main.tf
@@ -290,36 +290,36 @@ resource "aws_eks_access_entry" "manager" {
 }
 
 
-#---------------HPA----------------------------
+# #---------------HPA----------------------------
 
-data "aws_eks_cluster" "eks" {
-  name = aws_eks_cluster.eks.name
-}
+# data "aws_eks_cluster" "eks" {
+#   name = aws_eks_cluster.eks.name
+# }
 
-data "aws_eks_cluster_auth" "eks" {
-  name = aws_eks_cluster.eks.name
-}
+# data "aws_eks_cluster_auth" "eks" {
+#   name = aws_eks_cluster.eks.name
+# }
 
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks.token
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     host                   = data.aws_eks_cluster.eks.endpoint
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+#     token                  = data.aws_eks_cluster_auth.eks.token
+#   }
+# }
 
-resource "helm_release" "metrics_server" {
-  name = "metrics-server"
+# resource "helm_release" "metrics_server" {
+#   name = "metrics-server"
 
-  repository = "https://kubernetes-sigs.github.io/metrics-server/"
-  chart      = "metrics-server"
-  namespace  = "sb"
-  version    = "3.12.1"
+#   repository = "https://kubernetes-sigs.github.io/metrics-server/"
+#   chart      = "metrics-server"
+#   namespace  = "sb"
+#   version    = "3.12.1"
 
-  values = [file("${path.module}/values/metrics-server.yaml")]
+#   values = [file("${path.module}/values/metrics-server.yaml")]
 
-  depends_on = [aws_eks_node_group.general]
-}
+#   depends_on = [aws_eks_node_group.general]
+# }
 
 
 # #--------------Cluster Autoscaler---------------------------
