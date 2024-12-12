7482b6e36ecac9761da937e0d9fe43414585e358
Dolma <dolmatgurung1@gmail.com>
2024-12-11 21:53:03 -0500

    cluster autoscaler addon

--- Terraform/sb/main.tf
+++ Terraform/sb/main.tf
@@ -320,3 +320,11 @@ resource "helm_release" "metrics_server" {
 
   depends_on = [aws_eks_node_group.general]
 }
+
+
+#--------------Cluster Autoscaler---------------------------
+resource "aws_eks_addon" "pod_identity" {
+  cluster_name  = aws_eks_cluster.eks.name
+  addon_name    = "eks-pod-identity-agent"
+  addon_version = "v1.2.0-eksbuild.1"
+}
