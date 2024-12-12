dd5fb5216c389582320eb1bd0eaf73a168e69b50
Dolma <dolmatgurung1@gmail.com>
2024-12-11 23:44:29 -0500

    LB

--- Terraform/sb/main.tf
+++ Terraform/sb/main.tf
@@ -423,63 +423,63 @@ resource "helm_release" "cluster_autoscaler" {
 }
 
 
-# #-------------------LOADBALANCER------------------
+#-------------------LOADBALANCER------------------
 
-# data "aws_iam_policy_document" "aws_lbc" {
-#   statement {
-#     effect = "Allow"
+data "aws_iam_policy_document" "aws_lbc" {
+  statement {
+    effect = "Allow"
 
-#     principals {
-#       type        = "Service"
-#       identifiers = ["pods.eks.amazonaws.com"]
-#     }
+    principals {
+      type        = "Service"
+      identifiers = ["pods.eks.amazonaws.com"]
+    }
 
-#     actions = [
-#       "sts:AssumeRole",
-#       "sts:TagSession"
-#     ]
-#   }
-# }
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession"
+    ]
+  }
+}
 
-# resource "aws_iam_role" "aws_lbc" {
-#   name               = "${aws_eks_cluster.eks.name}-aws-lbc"
-#   assume_role_policy = data.aws_iam_policy_document.aws_lbc.json
-# }
+resource "aws_iam_role" "aws_lbc" {
+  name               = "${aws_eks_cluster.eks.name}-aws-lbc"
+  assume_role_policy = data.aws_iam_policy_document.aws_lbc.json
+}
 
-# resource "aws_iam_policy" "aws_lbc" {
-#   policy = file("./iam/AWSLoadBalancerController.json")
-#   name   = "AWSLoadBalancerController"
-# }
+resource "aws_iam_policy" "aws_lbc" {
+  policy = file("./iam/AWSLoadBalancerController.json")
+  name   = "AWSLoadBalancerController"
+}
 
-# resource "aws_iam_role_policy_attachment" "aws_lbc" {
-#   policy_arn = aws_iam_policy.aws_lbc.arn
-#   role       = aws_iam_role.aws_lbc.name
-# }
+resource "aws_iam_role_policy_attachment" "aws_lbc" {
+  policy_arn = aws_iam_policy.aws_lbc.arn
+  role       = aws_iam_role.aws_lbc.name
+}
 
-# resource "aws_eks_pod_identity_association" "aws_lbc" {
-#   cluster_name    = aws_eks_cluster.eks.name
-#   namespace       = "sb"
-#   service_account = "aws-load-balancer-controller"
-#   role_arn        = aws_iam_role.aws_lbc.arn
-# }
+resource "aws_eks_pod_identity_association" "aws_lbc" {
+  cluster_name    = aws_eks_cluster.eks.name
+  namespace       = "sb"
+  service_account = "aws-load-balancer-controller"
+  role_arn        = aws_iam_role.aws_lbc.arn
+}
 
-# resource "helm_release" "aws_lbc" {
-#   name = "aws-load-balancer-controller"
+resource "helm_release" "aws_lbc" {
+  name = "aws-load-balancer-controller"
 
-#   repository = "https://aws.github.io/eks-charts"
-#   chart      = "aws-load-balancer-controller"
-#   namespace  = "sb"
-#   version    = "1.7.2"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "sb"
+  version    = "1.7.2"
 
-#   set {
-#     name  = "clusterName"
-#     value = aws_eks_cluster.eks.name
-#   }
+  set {
+    name  = "clusterName"
+    value = aws_eks_cluster.eks.name
+  }
 
-#   set {
-#     name  = "serviceAccount.name"
-#     value = "aws-load-balancer-controller"
-#   }
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
 
-#   depends_on = [helm_release.cluster_autoscaler]
-# }
+  depends_on = [helm_release.cluster_autoscaler]
+}
