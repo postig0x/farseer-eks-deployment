a8bd6187e390da6c9ed2019bd8df6a43b64f557f
Dolma <dolmatgurung1@gmail.com>
2024-12-12 16:59:03 -0500

    reinfra

--- Terraform/Dev/main.tf
+++ Terraform/Dev/main.tf
@@ -359,78 +359,78 @@ resource "aws_iam_role" "cluster_autoscaler" {
   })
 }
 
-resource "aws_iam_policy" "cluster_autoscaler" {
-  name = "${aws_eks_cluster.eks.name}-cluster-autoscaler"
+# resource "aws_iam_policy" "cluster_autoscaler" {
+#   name = "${aws_eks_cluster.eks.name}-cluster-autoscaler"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "autoscaling:DescribeAutoScalingGroups",
-          "autoscaling:DescribeAutoScalingInstances",
-          "autoscaling:DescribeLaunchConfigurations",
-          "autoscaling:DescribeScalingActivities",
-          "autoscaling:DescribeTags",
-          "ec2:DescribeImages",
-          "ec2:DescribeInstanceTypes",
-          "ec2:DescribeLaunchTemplateVersions",
-          "ec2:GetInstanceTypesFromInstanceRequirements",
-          "eks:DescribeNodegroup"
-        ]
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "autoscaling:SetDesiredCapacity",
-          "autoscaling:TerminateInstanceInAutoScalingGroup"
-        ]
-        Resource = "*"
-      },
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "autoscaling:DescribeAutoScalingGroups",
+#           "autoscaling:DescribeAutoScalingInstances",
+#           "autoscaling:DescribeLaunchConfigurations",
+#           "autoscaling:DescribeScalingActivities",
+#           "autoscaling:DescribeTags",
+#           "ec2:DescribeImages",
+#           "ec2:DescribeInstanceTypes",
+#           "ec2:DescribeLaunchTemplateVersions",
+#           "ec2:GetInstanceTypesFromInstanceRequirements",
+#           "eks:DescribeNodegroup"
+#         ]
+#         Resource = "*"
+#       },
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "autoscaling:SetDesiredCapacity",
+#           "autoscaling:TerminateInstanceInAutoScalingGroup"
+#         ]
+#         Resource = "*"
+#       },
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "cluster_autoscaler" {
-  policy_arn = aws_iam_policy.cluster_autoscaler.arn
-  role       = aws_iam_role.cluster_autoscaler.name
-}
+# resource "aws_iam_role_policy_attachment" "cluster_autoscaler" {
+#   policy_arn = aws_iam_policy.cluster_autoscaler.arn
+#   role       = aws_iam_role.cluster_autoscaler.name
+# }
 
-resource "aws_eks_pod_identity_association" "cluster_autoscaler" {
-  cluster_name    = aws_eks_cluster.eks.name
-  namespace       = "${var.environment}"
-  service_account = "${var.environment}-cluster-autoscaler"
-  role_arn        = aws_iam_role.cluster_autoscaler.arn
-}
+# resource "aws_eks_pod_identity_association" "cluster_autoscaler" {
+#   cluster_name    = aws_eks_cluster.eks.name
+#   namespace       = "${var.environment}"
+#   service_account = "${var.environment}-cluster-autoscaler"
+#   role_arn        = aws_iam_role.cluster_autoscaler.arn
+# }
 
-resource "helm_release" "cluster_autoscaler" {
-  name = "autoscaler"
+# resource "helm_release" "cluster_autoscaler" {
+#   name = "autoscaler"
 
-  repository = "https://kubernetes.github.io/autoscaler"
-  chart      = "cluster-autoscaler"
-  namespace  = "${var.environment}"
-  version    = "9.37.0"
+#   repository = "https://kubernetes.github.io/autoscaler"
+#   chart      = "cluster-autoscaler"
+#   namespace  = "${var.environment}"
+#   version    = "9.37.0"
 
-  set {
-    name  = "rbac.serviceAccount.name"
-    value = "cluster-autoscaler"
-  }
+#   set {
+#     name  = "rbac.serviceAccount.name"
+#     value = "cluster-autoscaler"
+#   }
 
-  set {
-    name  = "autoDiscovery.clusterName"
-    value = aws_eks_cluster.eks.name
-  }
+#   set {
+#     name  = "autoDiscovery.clusterName"
+#     value = aws_eks_cluster.eks.name
+#   }
 
-  # MUST be updated to match your region 
-  set {
-    name  = "awsRegion"
-    value = "us-east-1"
-  }
+#   # MUST be updated to match your region 
+#   set {
+#     name  = "awsRegion"
+#     value = "us-east-1"
+#   }
 
-  depends_on = [helm_release.metrics_server]
-}
+#   depends_on = [helm_release.metrics_server]
+# }
 
 
 #-------------------LOADBALANCER------------------
