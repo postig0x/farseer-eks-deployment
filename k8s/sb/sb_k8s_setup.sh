1c50f4a35f4e513efb9801c8d7a72c41d6538f00
Dolma <dolmatgurung1@gmail.com>
2024-12-11 23:36:17 -0500

    recreate infra

--- k8s/sb/sb_k8s_setup.sh
+++ k8s/sb/sb_k8s_setup.sh
@@ -5,6 +5,9 @@ XAI_KEY="$1"
 SUBNET_IDS=$(cd Terraform/sb && terraform output  -json private_ips | jq -r 'join(",")')
 
 echo $SUBNET_IDS
+kubectl create namespace sb || echo "Namespace sb already exists"
+kubectl config set-context --current --namespace=sb
+aws eks update-kubeconfig --region us-east-1 --name sb-test
 
 # kubectl wait --for=condition=ready nodes --all --timeout=300s
 kubectl apply -f k8s/sb/roles/dev_role_binding.yaml
