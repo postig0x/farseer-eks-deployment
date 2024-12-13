24abf8d5c4619db1005381321316db30e63b6df7
Dolma <dolmatgurung1@gmail.com>
2024-12-13 17:00:16 -0500

    vpc peering

--- Terraform/Dev/modules/VPC/main.tf
+++ Terraform/Dev/modules/VPC/main.tf
@@ -235,35 +235,21 @@ resource "aws_route_table_association" "private-rt-association4" {
 # ##################################################
 # ### VPC PEERING ###
 # ##################################################
-# resource "aws_vpc_peering_connection" "peering" {
-#   peer_vpc_id = aws_vpc.vpc.id
-#   vpc_id      = data.aws_vpc.default.id
-#   auto_accept = true
-#   tags = {
-#     Name = "${var.environment}-${var.stacking}_vpc_peering"
-#   }
-# }
 
-# ##################################################
-# ### DEFAULT VPC ###
-# ##################################################
-# # Setting Default VPC
-# data "aws_vpc" "default" {
-#   default = true
-# }
+resource "aws_vpc_peering_connection" "peering" {
+  peer_vpc_id = aws_vpc.vpc.id
+  vpc_id      = data.aws_vpc.default.id
+  auto_accept = true
+  tags = {
+    Name = "${var.environment}-${var.stacking}_vpc_peering"
+  }
+}
 
-# # Data source to access the default route table of the default VPC
-# data "aws_route_table" "default" {
-#   vpc_id = data.aws_vpc.default.id
-# }
+##################################################
+### DEFAULT VPC ###
+##################################################
+# Setting Default VPC
+data "aws_vpc" "default" {
+  default = true
+}
 
-# # Add a route for VPC peering to the default route table
-# resource "aws_route" "vpc_peering_route" {
-#   route_table_id            = data.aws_route_table.default.id
-#   destination_cidr_block    = aws_vpc.vpc.cidr_block # Adjust based on peer VPC
-#   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
-#   lifecycle {
-#     create_before_destroy = true
-#     ignore_changes        = [destination_cidr_block]
-#   }
-# }
