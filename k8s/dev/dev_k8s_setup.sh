91b3f78e29a4c328904fd1d48bc2f55994ce22d1
Dolma <dolmatgurung1@gmail.com>
2024-12-13 17:02:22 -0500

    namespace

--- k8s/dev/dev_k8s_setup.sh
+++ k8s/dev/dev_k8s_setup.sh
@@ -6,7 +6,7 @@ SUBNET_IDS=$(cd Terraform/Dev && terraform output  -json private_ips | jq -r 'jo
 
 echo $SUBNET_IDS
 aws eks update-kubeconfig --region us-east-1 --name dev-green
-kubectl config set-context --current --namespace=dev
+kubectl config set-context --current --namespace=dev-green
 
 kubectl apply -f k8s/dev/roles
 
