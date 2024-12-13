dfa227babc392e346ef2167f488979585431be9a
Dolma <dolmatgurung1@gmail.com>
2024-12-12 21:45:02 -0500

    manager user

--- Terraform/sb/main.tf
+++ Terraform/sb/main.tf
@@ -198,96 +198,96 @@ resource "aws_eks_access_entry" "developer" {
 }
 
 
-# #---------------------MANAGER--------------
-# data "aws_caller_identity" "current" {}
+#---------------------MANAGER--------------
+data "aws_caller_identity" "current" {}
 
-# resource "aws_iam_role" "eks_admin" {
-#   name = "sb-test-eks-admin"
+resource "aws_iam_role" "eks_admin" {
+  name = "sb-test-eks-admin"
 
-#   assume_role_policy = <<POLICY
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Action": "sts:AssumeRole",
-#       "Principal": {
-#         "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-#       }
-#     }
-#   ]
-# }
-# POLICY
-# }
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      }
+    }
+  ]
+}
+POLICY
+}
 
-# resource "aws_iam_policy" "eks_admin" {
-#   name = "AmazonEKSAdminPolicy"
+resource "aws_iam_policy" "eks_admin" {
+  name = "AmazonEKSAdminPolicy"
 
-#   policy = <<POLICY
-# {
-#     "Version": "2012-10-17",
-#     "Statement": [
-#         {
-#             "Effect": "Allow",
-#             "Action": [
-#                 "eks:*"
-#             ],
-#             "Resource": "*"
-#         },
-#         {
-#             "Effect": "Allow",
-#             "Action": "iam:PassRole",
-#             "Resource": "*",
-#             "Condition": {
-#                 "StringEquals": {
-#                     "iam:PassedToService": "eks.amazonaws.com"
-#                 }
-#             }
-#         }
-#     ]
-# }
-# POLICY
-# }
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "eks:*"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "iam:PassRole",
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "iam:PassedToService": "eks.amazonaws.com"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
 
-# resource "aws_iam_role_policy_attachment" "eks_admin" {
-#   role       = aws_iam_role.eks_admin.name
-#   policy_arn = aws_iam_policy.eks_admin.arn
-# }
+resource "aws_iam_role_policy_attachment" "eks_admin" {
+  role       = aws_iam_role.eks_admin.name
+  policy_arn = aws_iam_policy.eks_admin.arn
+}
 
-# resource "aws_iam_user" "manager" {
-#   name = "manager"
-# }
+resource "aws_iam_user" "manager" {
+  name = "manager"
+}
 
-# resource "aws_iam_policy" "eks_assume_admin" {
-#   name = "AmazonEKSAssumeAdminPolicy"
+resource "aws_iam_policy" "eks_assume_admin" {
+  name = "AmazonEKSAssumeAdminPolicy"
 
-#   policy = <<POLICY
-# {
-#     "Version": "2012-10-17",
-#     "Statement": [
-#         {
-#             "Effect": "Allow",
-#             "Action": [
-#                 "sts:AssumeRole"
-#             ],
-#             "Resource": "${aws_iam_role.eks_admin.arn}"
-#         }
-#     ]
-# }
-# POLICY
-# }
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "sts:AssumeRole"
+            ],
+            "Resource": "${aws_iam_role.eks_admin.arn}"
+        }
+    ]
+}
+POLICY
+}
 
-# resource "aws_iam_user_policy_attachment" "manager" {
-#   user       = aws_iam_user.manager.name
-#   policy_arn = aws_iam_policy.eks_assume_admin.arn
-# }
+resource "aws_iam_user_policy_attachment" "manager" {
+  user       = aws_iam_user.manager.name
+  policy_arn = aws_iam_policy.eks_assume_admin.arn
+}
 
-# # Best practice: use IAM roles due to temporary credentials
-# resource "aws_eks_access_entry" "manager" {
-#   cluster_name      = aws_eks_cluster.eks.name
-#   principal_arn     = aws_iam_role.eks_admin.arn
-#   kubernetes_groups = ["my-admin"]
-# }
+# Best practice: use IAM roles due to temporary credentials
+resource "aws_eks_access_entry" "manager" {
+  cluster_name      = aws_eks_cluster.eks.name
+  principal_arn     = aws_iam_role.eks_admin.arn
+  kubernetes_groups = ["my-admin"]
+}
 
 
 # # ---------------HPA----------------------------
