28234318c6d6be9b93b0cc7b312083504a0a8726
jonwang22 <jon.wang2@gmail.com>
2024-12-12 23:34:06 -0500

    adding depends on statements for LBC

--- Terraform/sb/main.tf
+++ Terraform/sb/main.tf
@@ -453,6 +453,11 @@ resource "aws_iam_policy" "aws_lbc" {
 resource "aws_iam_role_policy_attachment" "aws_lbc" {
   policy_arn = aws_iam_policy.aws_lbc.arn
   role       = aws_iam_role.aws_lbc.name
+
+  depends_on = [
+    aws_iam_role.aws_lbc,
+    aws_iam_policy.aws_lbc
+  ]
 }
 
 resource "aws_eks_pod_identity_association" "aws_lbc" {
@@ -460,6 +465,10 @@ resource "aws_eks_pod_identity_association" "aws_lbc" {
   namespace       = "kube-system"
   service_account = "aws-load-balancer-controller"
   role_arn        = aws_iam_role.aws_lbc.arn
+
+  depends_on = [
+    aws_iam_policy_attachment.aws_lbc
+  ]
 }
 
 resource "helm_release" "aws_lbc" {
@@ -480,5 +489,7 @@ resource "helm_release" "aws_lbc" {
     value = "aws-load-balancer-controller"
   }
 
-  depends_on = [helm_release.cluster_autoscaler]
+  depends_on = [
+    helm_release.cluster_autoscaler,
+    aws_eks_pod_identity_association.aws_lbc]
 }
