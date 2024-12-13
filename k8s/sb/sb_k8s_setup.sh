77d60fe80915d6f924feedb2ed4d0f75020b8544
Dolma <dolmatgurung1@gmail.com>
2024-12-12 20:38:34 -0500

    recreate nodes

--- k8s/sb/sb_k8s_setup.sh
+++ k8s/sb/sb_k8s_setup.sh
@@ -10,12 +10,12 @@ kubectl create namespace sb || echo "Namespace sb already exists"
 kubectl config set-context --current --namespace=sb
 
 
-kubectl apply -f k8s/sb/roles/dev_role_binding.yaml
-kubectl apply -f k8s/sb/roles/admin_role_binding.yaml
+# kubectl apply -f k8s/sb/roles/dev_role_binding.yaml
+# kubectl apply -f k8s/sb/roles/admin_role_binding.yaml
 
 
-kubectl apply -f k8s/sb/secrets.yaml
-kubectl apply -f k8s/sb/backend
-kubectl apply -f k8s/sb/frontend
+# kubectl apply -f k8s/sb/secrets.yaml
+# kubectl apply -f k8s/sb/backend
+# kubectl apply -f k8s/sb/frontend
 
 
