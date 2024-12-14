8e5bd3f3852879526b166cde65a5d61b324ec732
Dolma <dolmatgurung1@gmail.com>
2024-12-13 23:06:13 -0500

    commenting subnets

--- k8s/green/Dev/Dev_k8s_setup.sh
+++ k8s/green/Dev/Dev_k8s_setup.sh
@@ -2,9 +2,9 @@
 
 XAI_KEY="$1"
 
-SUBNET_IDS=$(cd Terraform/green/Dev && terraform output  -json private_ips | jq -r 'join(",")')
+# SUBNET_IDS=$(cd Terraform/green/Dev && terraform output  -json private_ips | jq -r 'join(",")')
 
-echo $SUBNET_IDS
+# echo $SUBNET_IDS
 aws eks update-kubeconfig --region us-east-1 --name dev-green
 kubectl config set-context --current --namespace=dev-green
 
